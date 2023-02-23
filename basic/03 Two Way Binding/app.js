486e8e0c2dc9ea23440c02ba47c9e9544bbf13bf
Ata <agastyo2004@gmail.com>
2023-02-23 15:45:35 +0700

    add task 3

--- basic/03 Two Way Binding/app.js
+++ basic/03 Two Way Binding/app.js
@@ -3,18 +3,48 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      lastName: "",
+      fullname: "",
     };
   },
-  methods: {
-    showFullName() {
-      if (this.name === "") {
-        return "";
-      } else {
-        return this.name + " " + "Atallah";
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 2000);
       }
     },
+    name(value) {
+      if (value === "") {
+        return (this.fullname = "");
+      } else {
+        return (this.fullname = value + " " + this.lastName);
+      }
+    },
+    lastName(value) {
+      if (value === "") {
+        return (this.fullname = "");
+      } else {
+        return (this.fullname = this.name + " " + value);
+      }
+    },
+  },
+  computed: {
+    showFullName() {
+      console.log("render");
+      if (this.name === "" || this.lastName === "") {
+        return "";
+      } else {
+        return this.name + " " + this.lastName;
+      }
+    },
+  },
+  methods: {
     resetName() {
       this.name = "";
+      this.lastName = "";
     },
     add(num) {
       this.counter = this.counter + num;
---
+++ basic/task3/app.js
@@ -0,0 +1,37 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  watch: {
+    counter(value) {
+      if (value != 0) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 5000);
+      }
+    },
+  },
+  computed: {
+    messageResult() {
+      if (this.counter < 37) {
+        return "Not there yet";
+      } else if (this.counter === 37) {
+      } else {
+        return "Too much!";
+      }
+    },
+  },
+  methods: {
+    addFive() {
+      this.counter = this.counter + 5;
+    },
+    addOne() {
+      this.counter = this.counter + 1;
+    },
+  },
+});
+
+app.mount("#assignment");
