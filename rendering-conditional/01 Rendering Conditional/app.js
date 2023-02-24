1f55da5be5c87680fed385b001650f5822170795
Ata <agastyo2004@gmail.com>
2023-02-24 14:16:28 +0700

    remove list

--- rendering-conditional/01 Rendering Conditional/app.js
+++ rendering-conditional/01 Rendering Conditional/app.js
@@ -1,7 +1,18 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      inputGoal: "",
+      goals: [],
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.inputGoal);
+    },
+    removeGoal(idx) {
+      this.goals.splice(idx, 1);
+    },
   },
 });
 
-app.mount('#user-goals');
+app.mount("#user-goals");
