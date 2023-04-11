6c8414bd98e36e9d69a5c1d3ebed3197ab19d4ba
Ata <agastyo2004@gmail.com>
2023-04-11 09:57:46 +0700

    end task

---
+++ task-component-communication/vue-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <active-user :name="user.name" :age="user.age"></active-user>
+    <user-data @add-user="addContact"></user-data>
+  </div>
+</template>
+
+<script>
+import ActiveUser from "./components/ActiveUser.vue";
+import UserData from "./components/UserData.vue";
+export default {
+  components: { ActiveUser, UserData },
+  data() {
+    return {
+      user: {
+        name: "Ata",
+        age: "18",
+      },
+    };
+  },
+  methods: {
+    addContact(name, age) {
+      this.user = {
+        name: name,
+        age: age,
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ task-component-communication/vue-project/src/components/UserData.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <form @submit.prevent="submitData">
+      <div>
+        <label for="name">Name</label>
+        <input type="text" id="name" v-model="enteredName" />
+      </div>
+      <div>
+        <label for="name">Age</label>
+        <input type="text" id="age" v-model="enteredAge" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["add-user"],
+  data() {
+    return {
+      enteredName: "",
+      enteredAge: "",
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit("add-user", this.enteredName, this.enteredAge);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
